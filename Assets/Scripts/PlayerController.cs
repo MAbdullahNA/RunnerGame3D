4a7b9fca2d17a41aba8533c362a9b333bc1b63ba
Hussain Tech Labz <102964719+MAbdullahNA@users.noreply.github.com>
2025-03-20 15:26:11 +0500

    Detect collision and destroy coin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed = 5;
+    public float speed = 5;
+    public float turnSpeed, horizotalMove;
+
     //public Vector3 movement = new Vector3 (0, 0, 1);
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizotalMove = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizotalMove);
     }
-}
+    //
+    /*private void OnCollisionEnter(Collision collideObject)
+    {
+        if(collideObject.gameObject.tag == "Coin")
+        {
+            Debug.Log(" Collide with coin");
+            Destroy(collideObject.gameObject);
+        }
+    }*/
+    //
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Debug.Log(" Trigger with coin");
+            Destroy(other.gameObject);
+        }
+    }
+}                       //------------ Vector3.right = (1,0,0)
+                        //------------ Vector3.left = (-1,0,0)\
