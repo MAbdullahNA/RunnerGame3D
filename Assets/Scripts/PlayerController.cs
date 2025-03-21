2bea917b607edadabd4b6f2daca9441080400d44
Hussain Tech Labz <102964719+MAbdullahNA@users.noreply.github.com>
2025-03-21 16:59:34 +0500

    jump the player, stop game when collide with Enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,34 +2,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5;
-    public float turnSpeed, horizotalMove;
+    public int score = 0;
+    public float speed = 5f, jumpForce;
+    public float turnSpeed, horizotalMove, jumpInput;
+    public Rigidbody rb;
+    public bool isGrounded, gameOver = false;
 
     //public Vector3 movement = new Vector3 (0, 0, 1);
-
+    /*private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }*/
     // Update is called once per frame
     void Update()
     {
-        horizotalMove = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizotalMove);
+        if (gameOver == false)
+        {
+            // countinuousely move player forward
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            // Turn player left/right
+            horizotalMove = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizotalMove);
+            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+            // implement jump on player
+            jumpInput = Input.GetAxis("Jump");
+            if (jumpInput > 0 && isGrounded)
+            { rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z); }
+        }
     }
     //
-    /*private void OnCollisionEnter(Collision collideObject)
+    private void OnCollisionEnter(Collision collision)
     {
-        if(collideObject.gameObject.tag == "Coin")
+        if (collision.gameObject.tag == "Ground")
         {
-            Debug.Log(" Collide with coin");
-            Destroy(collideObject.gameObject);
+            isGrounded = true;
         }
-    }*/
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
     //
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Coin")
         {
+            score++;
+
+            Debug.Log(" Score : " + score);
             Debug.Log(" Trigger with coin");
             Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "Enemy")
+        {
+            gameOver = true;
         }
     }
 }                       //------------ Vector3.right = (1,0,0)
