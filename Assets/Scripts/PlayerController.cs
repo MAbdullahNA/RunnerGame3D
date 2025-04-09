43dd551d5f582b7df824edb40c3eaf62b2123ebc
Hussain Tech Labz <102964719+MAbdullahNA@users.noreply.github.com>
2025-04-09 17:01:39 +0500

    use newInput to move left/right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class PlayerController : MonoBehaviour
 {
+    //public InputSystem_Actions inputActions;
+    //InputAction move;
+    public InputAction moveInputAction;
+    Vector2 moveDirection;
+
     public int score = 0, highScore;
     public float speed = 5f, maxSpeed = 20f, jumpForce;
     public float turnSpeed, horizotalMove, jumpInput;
@@ -13,12 +20,26 @@ public class PlayerController : MonoBehaviour
     public Text scoreText, gameEndScore, highScoreText;
 
 
-    //public Vector3 movement = new Vector3 (0, 0, 1);
+    //public Vector3 moveDirection = new Vector3 (0, 0, 1);
     /*private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }*/
     // Update is called once per frame
+    private void OnEnable()
+    {
+        /*inputActions = new InputSystem_Actions();
+        move = inputActions.Player.Move;
+        move.Enable();*/
+        moveInputAction.Enable();
+    }
+    private void OnDisable()
+    {
+        //move.Disable();
+        moveInputAction.Disable();
+    }
+
+
     private void FixedUpdate()
     {
         jumpInput = Input.GetAxis("Jump");
@@ -32,14 +53,21 @@ public class PlayerController : MonoBehaviour
             if (speed < maxSpeed)
             { speed = speed + (.5f * Time.deltaTime); }
 
-            // countinuousely move player forward
+            /*// countinuousely move player forward
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
             // Turn player left/right
             horizotalMove = Input.GetAxis("Horizontal");
             transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizotalMove);
             //rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
-            // implement jump on player
-            
+            // implement jump on player*/
+            //moveDirection = move.ReadValue<Vector2>();
+            moveDirection = moveInputAction.ReadValue<Vector2>();
+            Debug.Log("moveDirection: "+moveDirection);
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * moveDirection.x);
+
         }
     }
     //
